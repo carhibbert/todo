3a33770c97d74db5dd1b9581ace3a749fcf8d816
Carissa Hibbert <108243658+carhibbert@users.noreply.github.com>
2022-08-27 14:53:49 -0500

    Add files via upload

--- NewToDo/script.js
+++ NewToDo/script.js
@@ -1,30 +1,3 @@
-// Add a new list item when "Add task" button is clicked
-function newTask(){
-    var list = document.createElement('li');
-    var input = document.getElementById("newTaskInput").value;
-    var inputTxt = document.createTextNode(input);
-    list.appendChild(inputTxt);
-    if (input ==="") {
-        alert("Please input a task!");
-    }
-    else{document.getElementById("tasks").appendChild(list)}
-
-    document.getElementById("newTaskInput".value = "");
-
-    var span = document.createElement("SPAN");
-    var text = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(text);
-    list.appendChild(span);
-
-    for(i=0; i< close.length; i++) {
-        close[i].onclick = function() {
-            var div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
-}
-
 // Append a close button to each list item
 var nodeList = document.getElementsByTagName("LI");
 var i;
@@ -43,9 +16,47 @@ for(i = 0; i<nodeList.length; i++){
         if (ev.target.tagName === 'LI') {
             ev.target.classList.toggle('checked');
         }
-        setTimeout(() => {
-            
-        }, 1000);
-    }, false);
+       
+    })
 
+    // Click on a close button to hide the current list item
+    var close = document.getElementsByClassName("close");
+    var i;
+    
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = setTimeout(remove, 5000);
+            function remove() {
+            var div = this.parentElement;
+            div.style.display = "none";
+            }
+    }
+
+
+
+// Add a new list item when "Add task" button is clicked
+function newTask(){
+    var list = document.createElement('li');
+    var input = document.getElementById("newTaskInput").value;
+    var inputTxt = document.createTextNode(input);
+    list.appendChild(inputTxt);
+    if (input ==="") {
+        alert("Please input a task!");
+    }
+    else{document.getElementById("tasks").appendChild(list)}
+
+    var span = document.createElement("SPAN");
+    var text = document.createTextNode("  \u00D7");
+    span.className = "close";
+    span.appendChild(text);
+    list.appendChild(span);
+
+    for(i=0; i< close.length; i++) {
+        close[i].onclick = function(ev) {
+            var div = this.parentElement;
+            div.style.display = "none";
+        } 
+    }
+
+    document.getElementById("newTaskInput").value='';
+}
 
